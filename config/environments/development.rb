ff6d7a8b44d0b9dd1f6362cd395c94297e9dae26
Graham Hadgraft <graham.hadgraft@gmail.com>
2020-07-11 15:55:13 +0100

    Switch to compatible pi functions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,5 +58,9 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
+end
+
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
 end
